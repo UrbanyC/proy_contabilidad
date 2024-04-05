2462ec99ae3553c7e3f2c886c44b1be8eaabc706
Savier Checo <saviercheco@hotmail.com>
2024-04-04 22:34:48 -0400

    login
    login

---
+++ programa_contabilidad/build/classes/contabilidad/fomulario/login2.rs
@@ -0,0 +1,6 @@
+contabilidad.fomulario.login$5
+contabilidad.fomulario.login
+contabilidad.fomulario.login$1
+contabilidad.fomulario.login$2
+contabilidad.fomulario.login$3
+contabilidad.fomulario.login$4
